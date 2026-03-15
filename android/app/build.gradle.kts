4a16337ff44a3de31559bb8982d499034c6fdb9f
SpaceJoy <89252350+SpacJoy@users.noreply.github.com>
2026-03-16 01:27:18 +0800

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
